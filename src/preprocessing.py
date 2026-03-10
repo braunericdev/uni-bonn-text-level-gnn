ca055fd70432fa0fb3c01bb2cd02fe0452682b8b
braune1 <s59ebrau@uni-bonn.de>
2026-03-10 17:15:45 +0100

    feature: read_corpus implementiert

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -38,3 +38,24 @@ def read_vocab(file_path: str | Path) -> Dict[str, int]:
         #'<pad> ist Konvention, da 'platzhalter' oder 'pad' (z.B. 'iPad') in Texten vorkommen könnten
         word2idx["<pad>"] = 0
     return word2idx
+
+def read_corpus(
+    file_path: str | Path, 
+    label2idx: Dict[str, int], 
+    word2idx: Dict[str, int]
+) -> Tuple[List[List[int]], List[int]]:
+    """
+    Liest den Textkorpus ein und codiert sowohl die Wörter als auch die Labels.
+    Erwartet eine tabulatorgetrennte Datei (Label \t Text).
+    """
+    with open(file_path, "r", encoding="utf-8") as f:
+        # splitlines() verhindert leere Strings durch den letzten Zeilenumbruch
+        content = [line.split('\t') for line in f.read().splitlines() if line.strip()]
+
+    # data: Jedes Wort im Text (x[1]) wird zu einer ID
+    data = [[encode_word(word, word2idx) for word in x[1].split()] for x in content]
+    
+    # gt (Ground Truth): Das Label (x[0]) wird zu einer ID
+    gt = [label2idx[x[0]] for x in content]
+
+    return data, gt
